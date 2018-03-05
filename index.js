db8a397472f8ff9a6846962567b58d939661fc63
NB_ASUS_RAMON\RAMON <rzabala1969@yahoo.es>
2018-03-05 19:38:17 +0100

    05/03/2018

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+var mongoose = require('mongoose'); //cargamos la libreria/modulo de mongoDB
+var app = require('./app');
+// configuramos un puerto para nuestro servidor
+var port = process.env.PORT || 3977;
+
+mongoose.connect('mongodb://localhost:27017/tfg_ucav', (err, res) => {
+
+	 if(err) {
+	 		throw err;
+	 }else {
+	 	console.log("Realizada conexion a base de datos correctamente...");
+	 	// ponemos el servidor a escuchar
+	 	app.listen(port, function(){
+	 		console.log("Servidor del api rest de postcast escuchando en http://localhost:" + port);
+	 	});
+	 }
+});
