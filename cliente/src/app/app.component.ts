6d0ae60c3c3813a63952f8049dbbe2efc91c5efc
NB_ASUS_RAMON\RAMON <rzabala1969@yahoo.es>
2018-04-03 00:18:36 +0200

    03/04/2018

--- cliente/src/app/app.component.ts
+++ cliente/src/app/app.component.ts
@@ -1,20 +1,133 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServicioUsuario } from  './services/servicio.usuario';
 import { Usuario } from './models/usuario';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers: [ServicioUsuario]
   
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   public title = 'PODCAST RADIO';
   public usuario: Usuario;
-  public identity = false;     //propiedad para comprobar los datos del usuario logueado , y lo vamos a guardar en el local storage.
+  public usuario_reg: Usuario;
+  public identity ;     //propiedad para comprobar los datos del usuario logueado , y lo vamos a guardar en el local storage.
+  public registrar = false;
+  public errorMessage;
   public token; //tb lo vamos a guardar en el local storage
 
-  constructor(){
+  constructor(
+    private _servicioUsuario:ServicioUsuario
+    ){
   	this.usuario = new Usuario('','','','','','ROLE_USER','');
     //this.usuario_registrado = new Usuario('','','','','',ROLE_USER','');
     //this.url = GLOBAL.url;
   }
 
+  ngOnInit(){
+     this.identity = this._servicioUsuario.getIdentity();  //regemos datos del localStorage
+    this.token = this._servicioUsuario.getToken();
+
+    console.log(this.identity);
+    console.log(this.token);
+  }
+
+
+  public onSubmit(){
+    console.log(this.usuario);
+    //datos del usuario identificado
+    this._servicioUsuario.login(this.usuario).subscribe(
+          response => {
+               let identity = response.usuario;
+               this.identity = identity; //usuario logueado que nos devuelve el API
+               console.log(this.identity);
+               if(!this.identity._id){
+                          alert("No se ha podido identificar el usuario");
+                      }else{
+                          // Guardar en el localstorage para tener al usuario sesión
+                          localStorage.setItem('identity', JSON.stringify(identity));
+
+                          // Conseguir el token para enviarselo a cada petición http
+                          this._servicioUsuario.login(this.usuario, 'true').subscribe(
+                              response => {
+                                  let token = response.token;
+                                  this.token = token;
+
+                                  if(this.token.length <= 0){
+                                      alert("Error al generar el token");
+                                  }else{
+                                      // Guardar en el localstorage con el  token 
+                                      localStorage.setItem('token', token);
+                                      this.usuario = new Usuario('','','','','','ROLE_USER','');
+                                  }
+                              },
+                              error => {
+                                var errorMessage = <any>error;
+
+                                if(errorMessage != null){
+                                  var body = JSON.parse(error._body);
+                                  this.errorMessage = body.message;
+
+                                  console.log(error);
+                                }
+                              }
+                            );
+
+
+              }
+
+
+
+          },
+         error => {
+        var errorMessage = <any>error;
+
+        if(errorMessage != null){
+          var body = JSON.parse(error._body);
+          this.errorMessage = body.message;
+
+          console.log(error);
+          }
+          }
+      );
+  }
+  
+
+
+
+  logout(){
+    //eliminamos todo del localStorage
+      
+     localStorage.removeItem('identity');
+     localStorage.removeItem('token');
+     localStorage.clear();
+     this.identity = null;
+     this.token = null;
+     //this._router.navigate(['/']);
+  }
+
+  registro(){
+    if (this.registrar){
+      this.registrar = false;
+
+    }else{
+      this.registrar = true;  
+    }
+ 
+        var element = document.getElementById("content");
+        var check = document.getElementById("reg");
+        if (this.registrar) {
+            element.style.display='block';
+        }
+        else {
+            element.style.display='none';
+        }
+
+
+
+
+
+  }
 }
+
+
